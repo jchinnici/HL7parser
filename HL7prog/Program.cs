7a7d5fdcbd25be0e145328b1c25a98b2b75bedd9
jchinnici <joseph.t.chinnici@gmail.com>
2019-03-19 18:55:13 -0400

    Add project files.

---
+++ HL7prog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
