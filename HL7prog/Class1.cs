cc71acd5e1600e7676402fed1ca70eb7a1e3ab5b
jchinnici <joseph.t.chinnici@gmail.com>
2019-03-19 20:03:32 -0400

    Parsed MSH segment

---
+++ HL7prog/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7prog
+{
+    class MSH
+    {
+        public string sendingApplication { get; set; }
+        public string sendingFacility { get; set; }
+        public string receivingFacility { get; set; }
+        public DateTime dateTime { get; set; }
+        public string messageType { get; set; }
+        public string messageID { get; set; }
+        public string processingID { get; set; }
+        public string encodingCharacters { get; set; }
+
+        public override string ToString()
+        {
+            return $"{encodingCharacters},{sendingFacility},{receivingFacility},{dateTime},{messageType},{messageID},{processingID},{sendingApplication}";
+        }
+    }
+}
--- HL7prog/Program.cs
+++ HL7prog/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,37 @@ namespace HL7prog
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            var currentFileData = File.ReadAllText("HL7.txt");
+            LoadDatabase(currentFileData);
+            Console.Read();
+
+
+
+        }
+        static void LoadDatabase(string text)
+        {
+            var lines = text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var details = line.Split('|');
+                var newRecord = new MSH();
+                newRecord.encodingCharacters = details[1];
+                newRecord.sendingApplication = details[2];
+                newRecord.sendingFacility = details[3];
+                newRecord.receivingFacility = details[5];
+                newRecord.dateTime = DateTime.ParseExact(details[6], "yyyyMMddHHmm", CultureInfo.CurrentCulture);
+                newRecord.messageType = details[7];
+                newRecord.messageID = details[8];
+                newRecord.processingID = details[9];
+
+                Console.Write(newRecord.ToString());
+                
+
+
+            }
         }
     }
 }
